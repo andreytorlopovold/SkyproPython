e082a522267b902080bc6d4aa5e1bc11edb731c1
Andrey Torlopov <i@atorlopov.ru>
2022-06-03 18:16:33 +0300

    Урок 3. Задание 2. Переделал цикл while на for

--- check_english/check_english.py
+++ check_english/check_english.py
@@ -4,35 +4,31 @@ if ready_answer != "ready":
     print("Кажется, вы не хотите играть. Очень жаль")
     exit(0)
 
-answers_count = 0
+answered_questions = 0
 questions = ["My name ___ Vova", "I ___ a coder", "I live ___ Moscow"]
 answers = ["is", "am", "in"]
 
 for i in range(len(questions)):
     print(f"Вопрос: {questions[i]}")
-    attemt = 3
+    
     # Добавили флаг, чтобы после неправильных ответов показать верный ответ.
     has_answer = False
-    
-    while True:
-        answer = input("Заполните пробел: ")    
-        attemt -= 1
-        # Тут делаем дополнительную проверку, чтобы пользователь не видел сообщение про 0 доступных попыток
-        if attemt == 0:
-            break
         
-        if answer == answers[i]:
-            answers_count += 1
+    for j in range(3, 0, -1):
+        attempts_left = j -1
+        user_answer = input("Заполните пробел: ")    
+        if user_answer == answers[i]:
+            answered_questions += 1
             has_answer = True
             print("Ответ верный!")
             break
-        else:
-            print(f"Осталось попыток: {attemt}, попробуйте еще раз!")
+        elif attempts_left > 0:
+            print(f"Осталось попыток: {attempts_left}, попробуйте еще раз!")
     
     if has_answer == False:
         print(f"Неправильно. \nПравильный ответ: {answers[i]}")
     print("")
 
 question_count = len(questions)
-percent = round(100 * answers_count / len(questions), 0)
-print(f"Вот и все! Вы ответили на {answers_count} вопросов из {question_count} верно, это {percent} процентов.")
+percent = round(100 * answered_questions / len(questions), 2)
+print(f"Вот и все! Вы ответили на {answered_questions} вопросов из {question_count} верно, это {percent} процентов.")
