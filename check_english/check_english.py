e868d6099da8c5d2d533a2608dcec3ef49b01677
Andrey Torlopov <i@atorlopov.ru>
2022-06-03 17:59:14 +0300

    Урок 3. Задание 2. Добавил комментарии

--- check_english/check_english.py
+++ check_english/check_english.py
@@ -11,10 +11,13 @@ answers = ["is", "am", "in"]
 for i in range(len(questions)):
     print(f"Вопрос: {questions[i]}")
     attemt = 3
+    # Добавили флаг, чтобы после неправильных ответов показать верный ответ.
     has_answer = False
+    
     while True:
         answer = input("Заполните пробел: ")    
         attemt -= 1
+        # Тут делаем дополнительную проверку, чтобы пользователь не видел сообщение про 0 доступных попыток
         if attemt == 0:
             break
         
@@ -25,7 +28,7 @@ for i in range(len(questions)):
             break
         else:
             print(f"Осталось попыток: {attemt}, попробуйте еще раз!")
-            # 
+    
     if has_answer == False:
         print(f"Неправильно. \nПравильный ответ: {answers[i]}")
     print("")
