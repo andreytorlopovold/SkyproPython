bd51d50a99296338387ada2c421637f9bf529e94
Andrey Torlopov <i@atorlopov.ru>
2022-06-16 16:15:35 +0300

    Курс 1. Курсовая. Работа над ошибками

--- course_work/morse.py
+++ course_work/morse.py
@@ -1,63 +1,6 @@
 import random
+from morse_dict import morse_dict
 
-# Справочная информация
-morse_dict = {
-    "0": "-----",
-    "1": ".----",
-    "2": "..---",
-    "3": "...--",
-    "4": "....-",
-    "5": ".....",
-    "6": "-....",
-    "7": "--...",
-    "8": "---..",
-    "9": "----.",
-    "a": ".-",
-    "b": "-...",
-    "c": "-.-.",
-    "d": "-..",
-    "e": ".",  
-    "f": "..-.",
-    "g": "--.",
-    "h": "....",
-    "i": "..",
-    "j": ".---",
-    "k": "-.-",
-    "l": ".-..",
-    "m": "--",
-    "n": "-.",
-    "o": "---",
-    "p": ".--.",
-    "q": "--.-",
-    "r": ".-.",
-    "s": "...",
-    "t": "-",
-    "u": "..-",
-    "v": "...-",
-    "w": ".--",
-    "x": "-..-",
-    "y": "-.--",
-    "z": "--..",
-    ".": ".-.-.-",
-    ",": "--..--",
-    "?": "..--..",
-    "!": "-.-.--",
-    "-": "-....-",
-    "/": "-..-.",
-    "@": ".--.-.",
-    "(": "-.--.",
-    ")": "-.--.-"
-}
-
-# Приветствие
-print('Сегодня мы потренируемся расшифровывать азбуку Морзе')
-# input('Нажмите Enter и начнем')
-
-# Список вопросов
-questions = ['snake', 'spring', 'sos', 'help', 'day']
-
-# Тут храним бульки с ответами
-answers = []
 
 def morse_endoce(word):
     """Translate word into morse alphabet
@@ -68,29 +11,48 @@ def morse_endoce(word):
     Returns:
         String: Morse string. Letters with 1 space separator.
     """
-    return ' '.join(list(map(lambda x: morse_dict[x], word.lower())))
+    return [morse_dict[letter] for letter in word]
 
-def get_word():
+
+def get_word(words):
     """Return random word from variable with question words 
 
     Returns:
         String: return random word from 'questions'
     """
-    return random.sample(questions, 1)[0]
+    return random.choice(words)
 
 
-# Режим проверки 
+def get_statictic(answers):
+    print()
+    print(f'Всего задачек: {len(answers)}')
+    print(f'Отвечено верно: {answers.count(True)}')
+    print(f'Отвечено неверно: {answers.count(False)}')
 
-for i in range(1,6):
-    word = get_word()
-    encoded_word = morse_endoce(word)
-    print(f'Слово {i}: {encoded_word}')
-    user_answer = input('Ваш ответ: ')
-    answers.append(user_answer == word)
-    print(f'Верно, {word}!' if user_answer == word else f'Неверно, {word}!')
 
-print()
-print(f'Всего задачек: {len(questions)}')
-print(f'Отвечено верно: {answers.count(True)}')
-print(f'Отвечено неверно: {answers.count(False)}')
+def main():
+    # Приветствие
+    print('Сегодня мы потренируемся расшифровывать азбуку Морзе')
+    # input('Нажмите Enter и начнем')
 
+    # Список вопросов
+    questions = ['snake', 'spring', 'sos', 'help', 'day']
+
+    # Тут храним бульки с ответами
+    answers = []
+
+    # Режим проверки
+    for i in range(1, 6):
+        word = get_word(questions)
+        encoded_word = morse_endoce(word)
+        print(f'Слово {i}: {encoded_word}')
+        user_answer = input('Ваш ответ: ')
+        answers.append(user_answer == word)
+        print(f'Верно, {word}!' if user_answer ==
+              word else f'Неверно, {word}!')
+
+    get_statictic(answers)
+
+
+if __name__ == "__main__":
+    main()
